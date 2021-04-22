8980ee6ea8df8b1d5b34ae1995f6d284ee71187b
DanielThorson <dragonflydan@gmail.com>
2021-04-21 23:29:00 -0700

    added rough js functionality to Inventory page

--- Main.js
+++ Main.js
@@ -1,24 +1,37 @@
-let x = document.getElementById("revenue");
-
-let result = document.createElement("h3");
-result.innerText = "$100.00";
-x.prepend(result);
-
+// for Revenue Page
 fetch("https://donutshop-api.herokuapp.com/revenue?id=234", {
   method: "GET",
 })
   .then((response) => response.json())
   .then((data) => {
-    alert(
-      `Dank Donuts Revenue:$${JSON.stringify(
-        data.revenue.toLocaleString("en-us", {
-          style: "currency",
-          currency: "usd",
-        })
-      )}`
-    );
-    console.log(data);
-    getInput();
+    let x = document.getElementById("revenue");
+
+    let result = document.createElement("h2");
+    result.innerText = data.revenue.toLocaleString("en-us", {
+      style: "currency",
+      currency: "usd",
+    });
+    x.prepend(result);
+    // console.log(data);
+    // getInput();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// for inventory page
+fetch("https://donutshop-api.herokuapp.com/inventory?id=234", {
+  method: "GET",
+})
+  .then((response) => response.json())
+  .then((data) => {
+    let x = document.getElementById("inventory");
+
+    let result = document.createElement("h5");
+    result.innerText = JSON.stringify(data.donuts);
+    x.prepend(result);
+    // console.log(data.donuts);
+    // getInput();
   })
   .catch((err) => {
     console.error(err);
