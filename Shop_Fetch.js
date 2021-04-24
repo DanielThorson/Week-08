644c343bd52402bb93f0f069a1bc9f514dba8c7b
Daniel Thorson <81667518+DanielThorson@users.noreply.github.com>
2021-04-24 12:08:53 -0700

    Add files via upload

--- Shop_Fetch.js
+++ Shop_Fetch.js
@@ -6,33 +6,53 @@ fetch("https://donutshop-api.herokuapp.com/shops/", {
   .then((response) => response.json())
   .then((data) => {
     let x = document.getElementById("shops");
-    let result = document.createElement("option");
-    result.innerText = data;
-    x.append(result);
-    // let x = document.getElementById("shops");
-    // let result = document.createElement("h5");
-    // result.innerText = JSON.stringify(data);
-    // x.prepend(result);
+    for (var i = 0; i < data.length; i++) {
+      let result = document.createElement("option");
+      result.innerText = data[i]
+      result.value = data[i];
+      x.append(result);
+      console.log(result);
+    }
   })
   .catch((err) => {
     console.error(err);
   });
 
+// var shops = [];
 
-  // fetch("https://donutshop-api.herokuapp.com/shops/", {
-  //   method: "GET",
-  // })
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     shops = data;
-  //     let x = document.getElementById("shops");
-  //     let result = document.createElement("option");
-  //     var html = "<option>";
-  //     for (var i = 0; i < shops.length; i++) {
-  //     html += "<option>" + shops[i] + "</option>";
-  //     document.getElementById("shops").innerHTML = html;
-  //     x.prepend(result);
-  //   }
-  //   .catch((err) => {
-  //     console.error(err);
-  //   });
+// fetch("https://donutshop-api.herokuapp.com/shops/", {
+//   method: "GET",
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     shops = data;
+//     let x = document.getElementById("#shops");
+//     let result = document.createElement("option");
+//     result.innerText = shops;
+//     for (var i = 0; i < shops.length; i++) {
+//       html += "<option>" + shops[i] + "</option>";
+//       document.getElementById("#shops").innerHTML = html;
+//       x.prepend(data);
+
+//   }})
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+// fetch("https://donutshop-api.herokuapp.com/shops/", {
+//   method: "GET",
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     shops = data;
+//     let x = document.getElementById("shops");
+//     let result = document.createElement("option");
+//     var html = "<option>";
+//     for (var i = 0; i < shops.length; i++) {
+//     html += "<option>" + shops[i] + "</option>";
+//     document.getElementById("shops").innerHTML = html;
+//     x.prepend(result);
+//   }
+//   .catch((err) => {
+//     console.error(err);
+//   });
